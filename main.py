03a04a7ddd976945a68ee8808e3d57d6bfcd5adc
Lucas <lucasbecca3@hotmail.com>
2024-03-03 00:02:16 -0300

    cambios

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-
+import uvicorn
 
 
 app = FastAPI()
 
 
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:routers",host="localhost", reload=True)
--- schemas.py
+++ schemas.py
@@ -2,10 +2,11 @@ from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
+    id: int
     name: str
     surname: str
     age: int
     hotmail: str
 
     class Config:
-        orm_mode = True
+        form_attributes = True
